b42b482b44784a2d5b10d4ae46a8ebaca65d165d
chenquanjun <chenshiyouxiang@gmail.com>
2014-09-15 12:10:13 +0800

    stenchmode

--- scripts/app/scenes/HighLightArea.lua
+++ scripts/app/scenes/HighLightArea.lua
@@ -15,10 +15,18 @@ HighLightArea.__index      = HighLightArea
     ---Init Method-----
     ---------------------]]
 
-function HighLightArea:create(params)
+--混合模式
+function HighLightArea:createBlend(params)
 	local ret = HighLightArea.new()
-	ret:init(params)
+	ret:initBlend(params)
 	return ret
+end
+
+--裁剪模式
+function HighLightArea:createStencil(params)
+    local ret = HighLightArea.new()
+    ret:initStencil(params)
+    return ret
 end
 
 --[[
@@ -36,7 +44,8 @@ end
     }
 ]]--
 
-function HighLightArea:init(params)
+--混合模式
+function HighLightArea:initBlend(params)
     --size 与 position使用默认值，效果则为全屏的遮罩
     local size = params.contentSize or CCSize(display.width, display.height) --整个高亮区域尺寸，默认全屏
     local position = params.position or ccp(display.cx, display.cy) --默认屏幕中间
@@ -78,6 +87,57 @@ function HighLightArea:init(params)
     self:setTexture(pRt:getSprite():getTexture())
     self:setPosition(position)
     self:setFlipY(true)
+end
+
+--裁剪模式
+function HighLightArea:initStencil(params)
+    --size 与 position使用默认值，效果则为全屏的遮罩
+    local size = params.contentSize or CCSize(display.width, display.height) --整个高亮区域尺寸，默认全屏
+    local position = params.position or ccp(display.cx, display.cy) --默认屏幕中间
+
+    local bgColor = params.bgColor or ccc3(255, 0, 0) --非高亮区域颜色
+    local bgOpacity = params.bgOpacity or 0.6 --非高亮区域透明度
+    local rectArray = params.rectArray
+
+    local layerColor = CCLayerColor:create(ccc4(bgColor.r, bgColor.g, bgColor.b, bgOpacity * 255), size.width, size.height)
+
+    local clipNode = CCClippingNode:create();
+    clipNode:setInverted(true)--设定遮罩的模式true显示没有被遮起来的纹理   如果是false就显示遮罩起来的纹理  
+    clipNode:setAlphaThreshold(0) --设定遮罩图层的透明度取值范围 
+    clipNode:addChild(layerColor)
+    self:addChild(clipNode)
+
+    local batchNode = CCSpriteBatchNode:create("Images/circle.png")
+
+    local circleSpr = CCSprite:createWithTexture(batchNode:getTexture())
+    local circleSize = circleSpr:getContentSize()
+
+    local widthPara 
+    local heightPara
+
+    for i, rect in ipairs(rectArray) do
+        local circleSpr = CCSprite:createWithTexture(batchNode:getTexture())
+
+        if not widthPara then
+            local circleSize = circleSpr:getContentSize()
+
+            --宽度和高度参数，1.4142为根号2，矩形的外接椭圆的长轴与短轴长度
+            widthPara = 1.4142 / circleSize.width
+            heightPara = 1.4142 / circleSize.height
+        end
+
+        local fScaleX = widthPara * rect.size.width
+        local fScaleY = heightPara * rect.size.height
+        circleSpr:setScaleX(fScaleX)
+        circleSpr:setScaleY(fScaleY)
+        circleSpr:setPosition(rect:getMidX(), rect:getMidY())
+        batchNode:addChild(circleSpr)
+    end
+ 
+    clipNode:setStencil(batchNode)
+    
+    --锚点是(0, 0),所以这里要转换一下
+    self:setPosition(position.x - size.width * 0.5, position.y - size.height * 0.5)
 end
 
 
--- scripts/app/scenes/MainScene.lua
+++ scripts/app/scenes/MainScene.lua
@@ -4,6 +4,7 @@ local MainScene = class("MainScene", function()
 end)
 
 function MainScene:ctor()
+    --背景颜色
     local layerColor = display.newColorLayer(ccc4(255, 255, 255, 255))
     self:addChild(layerColor)
 
@@ -11,7 +12,7 @@ function MainScene:ctor()
 
     for i = 1, 3 do
         local bgSprite = CCSprite:create("Icon@2x.png")
-        bgSprite:setPosition(display.left + i * 350 - 200, display.cy)
+        bgSprite:setPosition(display.left + i * 200 - 100, display.cy)
         self:addChild(bgSprite)
         spriteVec[i] = bgSprite
         bgSprite:setScaleY(i)
@@ -19,6 +20,55 @@ function MainScene:ctor()
     end
 
     local HighLightArea = require("app.scenes.HighLightArea")
+
+    --简单的触摸方法
+    local function addSimpleTouch(sprite, callback)
+        sprite:addTouchEventListener(function(event, x, y)
+            if event == "began" then
+                sprite:setScale(sprite:getScale() * 0.9)
+                return true -- catch touch event, stop event dispatching
+            end
+
+            local touchInSprite = sprite:getCascadeBoundingBox():containsPoint(CCPoint(x, y))
+            if event == "ended" then
+                sprite:setScale(sprite:getScale() / 0.9)
+                if touchInSprite then 
+                    callback()
+                end
+            end
+        end)
+    end
+
+    local highLightDic = {}
+    local curIndex = 1
+
+    local infoLabel =CCLabelTTF:create("当前模式: Origin", "Arial", 30)
+    infoLabel:setPosition(display.left + 50, display.top - 50)
+    infoLabel:setAnchorPoint(ccp(0, 0.5))
+    infoLabel:setColor(ccc3(0, 0, 255))
+    self:addChild(infoLabel, 100)
+
+    --添加label
+    local function addLabel(str)
+        local label = CCLabelTTF:create(str, "Arial", 30)
+        self:addChild(label, 30)
+        label:setColor(ccc3(255, 0, 0))
+        label:setTouchEnabled(true)
+        label:setPosition(display.right - 100, display.top - 100 - 40 * curIndex)
+        addSimpleTouch(label, function ()
+            for key, sprite in pairs(highLightDic) do
+                local visible = false
+                if key == str then
+                    visible = true
+                end
+                sprite:setVisible(visible)
+            end
+            infoLabel:setString("当前模式: "..str)
+        end)
+        curIndex = curIndex + 1
+    end
+
+    --初始化变量
     local params = {
             contentSize = CCSize(display.width, display.height),
             position = ccp(display.cx, display.cy),
@@ -36,9 +86,30 @@ function MainScene:ctor()
         params.rectArray[#params.rectArray + 1] = sprite:getCascadeBoundingBox()
     end
 
-    local highSpr = HighLightArea:create(params)
-    self:addChild(highSpr)
+    do --原始模式
+        local modeName =  "Origin"
+        addLabel(modeName)
+    end
 
+    do --混合模式
+        local highSpr = HighLightArea:createBlend(params)
+        self:addChild(highSpr)   
+        highSpr:setVisible(false)
+
+        local modeName =  "BlendMode"
+        highLightDic[modeName] = highSpr
+        addLabel(modeName)
+    end
+
+    do --裁剪模式
+        local highSpr = HighLightArea:createStencil(params)
+        self:addChild(highSpr)
+        highSpr:setVisible(false)
+
+        local modeName =  "StencilMode"
+        highLightDic[modeName] = highSpr
+        addLabel(modeName)
+    end
 end
 
 return MainScene
